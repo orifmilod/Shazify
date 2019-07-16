451f3a046d69a2da72419435229ab0dfaec13be1
jkey-app <jkey.app@gmail.com>
2019-07-16 23:25:30 +0200

    Review and refactor

--- client/src/App.js
+++ client/src/App.js
@@ -5,56 +5,51 @@ import Profile from './common/profile';
 import SearchPage from './SearchPage';
 import Home from './Home';
 
+class App extends React.Component {
+  state = {
+    userData: {},
+  };
 
-class App extends React.Component 
-{
-    state = 
-    {
-        userData: { },
-    }
+  getAccessToken = () => {
+    const hash = queryString.parse(window.location.hash);
+    return hash.access_token;
+  };
 
-    getAccessToken = () => {
-        let hash = queryString.parse(window.location.hash);
-        return hash.access_token;
-    }
+  componentDidMount() {
+    const access_token = this.getAccessToken();
+    if (access_token) this.getUserData();
+  }
 
-    componentDidMount()
-    {
-        let access_token = this.getAccessToken();
-        if(access_token)
-            this.getUserData();
-    }
+  getUserData = e => {
+    const hash = queryString.parse(window.location.hash);
+    fetch('https://api.spotify.com/v1/me', {
+      headers: { Authorization: `Bearer ${hash.access_token}` },
+    })
+      .then(data => data.json())
+      .then(userData => this.setState({ userData }))
+      .catch(err => console.error(err));
+  };
 
-    getUserData = (e) => {
-        let hash = queryString.parse(window.location.hash);
-        fetch('https://api.spotify.com/v1/me', { 
-            headers: { 'Authorization': `Bearer ${hash.access_token}` }
-        })
-        .then(data => data.json())
-        .then(userData => this.setState({ userData }))
-        .catch(err => console.error(err)) 
-    }
-
-    render() {
-        const { userData } = this.state;
-        return (
-            <div className="App">
-            {   
-                Object.keys(userData).length > 0 ? 
-                <div className='row'>
-                    <div className='col-3 bg-success vh-100 text-left'>
-                        <Profile userData={userData}/>
-                    </div>
-                   
-                    <div className='col-6 bg-light scroll-able'>
-                        <SearchPage handleSearch={this.handleSearch}/>
-                    </div>
-                </div>
-                :
-                <Home getUserData={this.getUserData}/>
-            }
+  render() {
+    const { userData } = this.state;
+    
+    return (
+      <div className="App">
+        {Object.keys(userData).length > 0 ? (
+          <div className="row">
+            <div className="col-3 bg-success vh-100 text-left">
+              <Profile userData={userData} />
             </div>
-        )
-    }
-} 
+
+            <div className="col-6 bg-light scroll-able">
+              <SearchPage handleSearch={this.handleSearch} />
+            </div>
+          </div>
+        ) : (
+          <Home getUserData={this.getUserData} />
+        )}
+      </div>
+    );
+  }
+}
 export default App;
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-let express = require('express'); // Express web server framework
-let cors = require('cors');
-let querystring = require('querystring');
-let cookieParser = require('cookie-parser');
-let redirect_uri = 'https://ispotify.herokuapp.com/'; // Your redirect uri
-let path = require('path');
+const express = require('express'); // Express web server framework
+const cors = require('cors');
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+const redirect_uri = 'https://ispotify.herokuapp.com/'; // Your redirect uri
+const path = require('path');
 
-let generateRandomString = function(length) {
+const generateRandomString = function(length) {
   let text = '';
-  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
   for (let i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -15,49 +16,48 @@ let generateRandomString = function(length) {
   return text;
 };
 
-let stateKey = 'spotify_auth_state';
+const stateKey = 'spotify_auth_state';
 
-let app = express();
+const app = express();
 
-app.use(express.static(__dirname + '/public'))
-   .use(cors())
-   .use(cookieParser());
+app
+  .use(express.static(__dirname + '/public'))
+  .use(cors())
+  .use(cookieParser());
 
 app.get('/login', (req, res) => {
-
   let state = generateRandomString(16);
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  let scope = 'user-read-private user-read-email user-read-playback-state';
-  let redirectURL = 'https://accounts.spotify.com/authorize?' +
-  querystring.stringify({
-    response_type: 'token',
-    client_id: process.env.CLIENT_ID,
-    scope: scope,
-    redirect_uri: redirect_uri,
-    state: state
-  });
-  console.log(redirectURL)
+  const scope = 'user-read-private user-read-email user-read-playback-state';
+  const redirectURL =
+    'https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'token',
+      client_id: process.env.CLIENT_ID,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state,
+    });
+  console.log(redirectURL);
   res.redirect(redirectURL);
 });
 
-
 //Serve our static asset if in production
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    
-    app.get('*', (req, res) => {
-      res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
-  else {
-      app.use(express.static(path.join(__dirname, '/client/public')));
-      app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "./client/public/index.html"));
-      });
-  }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-let PORT = process.env.PORT || 8888
+  app.get('*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+} else {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, './client/public/index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 8888;
 
 app.listen(PORT, () => `Server is running on port ${PORT}`);
