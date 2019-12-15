254eaec0b30f4714048e7908a80a37aaa3846c2e
milad440550 <milad440550@gmail.com>
2019-12-15 15:45:33 +0100

    Added tracker

--- frontend/src/components/Content/index.js
+++ frontend/src/components/Content/index.js
@@ -1,30 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import { withRouter, Route } from "react-router-dom";
 
 import SearchedTrackList from "../SearchedTrackList";
 import FeaturedPlaylist from "../FeaturedPlaylist";
 import Playlist from "../Playlist";
 
-//Styled components
 import Grid from "../../styled/Grid";
 
-class Content extends Component {
-  render() {
-    const { playTrack } = this.props;
-    return (
-      <Grid overflow="auto">
-        <Route path="/home" exact render={props => <FeaturedPlaylist {...props} />} />
-        <Route
-          path="/home/search/:searchedTrack"
-          render={props => (<SearchedTrackList {...props} playTrack={playTrack} />)}
-        />
-        <Route
-          path="/home/playlist/:playlistID"
-          render={props => <Playlist {...props} playTrack={playTrack} />}
-        />
-      </Grid>
-    );
-  }
+function Content({ playTrack }) {
+  return (
+    <Grid overflow="auto">
+      <Route path="/home" exact render={props => <FeaturedPlaylist {...props} />} />
+      <Route
+        path="/home/search/:searchedTrack"
+        render={props => (<SearchedTrackList {...props} playTrack={playTrack} />)}
+
+      />
+      <Route
+        path="/home/playlist/:playlistID"
+        render={props => <Playlist {...props} playTrack={playTrack} />}
+      />
+    </Grid>
+  );
 }
 
 export default withRouter(Content);
